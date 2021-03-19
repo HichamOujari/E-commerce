6944b658503f0fd182c8828dc12f8613b54087c2
unknown <ayouboukrirou@gmail.com>
2021-03-19 12:49:38 +0100

    by  oukror

--- pages/about.js
+++ pages/about.js
@@ -5,23 +5,23 @@ import React, { Component } from 'react';
 import Nav from "../components/nav"
 import Footer from "../components/footer"
 import ReactPlayer from 'react-player'
+import Image from  'next/image'
 
-export default function about() {
+
+export default function about(brnads) {
     return (
         <div className={style.apropos}>
             <Nav/>
             <div className={style.part1}>
                 <p>A PROPOS DE NOUS</p> 
             </div>
+    
             <div className={style.part2}>
                 <div className={style.leftside}>
                     <p>
                     Le Groupe SAHARA MARBRE fondé en 1983 est basé à  <strong>RABAT, TEMARA , CASABLANCA, MARRAKECH ET NADOR</strong>  Entreprise familiale d’une centaine d’employés, nous perpétuons un savoir faire détenu depuis Trois générations de marbriers alliant tradition et modernité.
-
 Reconnu pour la maîtrise dans l’extraction, la commercialisation, l’import et l’export de tous types de Marbre,Granite et Pierre, SAHARA MARBRE est également plébiscité pour la réalisation de plans de travail de cuisine, revêtements de sol et mur pour salle de bain, bac à douche à l’Italienne, vasque, dallage…
-
 Toujours plus originales et complexes, les matières travaillées sont à la fois nobles, naturelles et intemporelles : Pierres, Marbres, Granits, Ardoises, Onyx, Agate … Mais aussi novateurs : Quartz, Silestone….
-
 De dimension humaine et familiale, nous disposons d’ateliers à la pointe de la technique, d’un stock de matières sans cesse renouvelées et de nos propres équipes formées à notre savoir-faire.
 tP
 Afin d’affiner votre choix, nous vous proposons plusieurs sites combinant showrooms, parc à tranches et ateliers.
@@ -30,8 +30,21 @@ Afin d’affiner votre choix, nous vous proposons plusieurs sites combinant show
                 <div className={style.rightside}>
                     <ReactPlayer url="https://www.youtube.com/watch?v=LcXuM3DTa3M&t=11s&ab_channel=OpteamRH" ></ReactPlayer>
                 </div>
-                <div className={style.References}></div>
             </div>
+            <div className={style.References}>
+                    <div>Nos References </div>
+                    <div className={style.Brands} >
+                       <Image src="/../public/About/caree.png"   width={100} height={100}        />
+                       <Image src="/../public/About/caree.png"   width={100} height={100}        />
+                       <Image src="/../public/About/caree.png"   width={100} height={100}        />
+                       <Image src="/../public/About/caree.png"   width={100} height={100}        />
+                       <Image src="/../public/About/caree.png"   width={100} height={100}        />
+                       <Image src="/../public/About/caree.png"   width={100} height={100}        />
+                       <Image src="/../public/About/caree.png"   width={100} height={100}        />
+                       <Image src="/../public/About/caree.png"   width={100} height={100}        />
+                       <Image src="/../public/About/caree.png"   width={100} height={100}        />
+                    </div>
+                </div>
             <Footer/>
         </div>
     )
